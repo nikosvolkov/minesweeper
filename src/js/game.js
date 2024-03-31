7fe322b35353fcf6459a45412bb3d7e57dc8b1c4
Volkov Nikita <crystalnik123@gmail.com>
2024-03-31 14:23:26 +0300

    Added flag button and it's logic

--- src/js/game.js
+++ src/js/game.js
@@ -2,6 +2,8 @@
 const rows = 10;
 const columns = 10;
 
+let flagEnabled = false;
+
 const gameDiv =  document.getElementById('app')
 
 const createBoard = () =>{
@@ -14,7 +16,38 @@ const createBoard = () =>{
     for(let c = 0; c < columns; c++){
       const boardCell = document.createElement('div')
       boardCell.id = `${r}-${c}`;
+      boardCell.addEventListener('click', cellClicked)
       boardDiv.append(boardCell)
+    }
+  }
+}
+
+const createFlagButton = () =>{
+  const flagButton = document.createElement('button')
+  flagButton.classList.add('flag-btn')
+  flagButton.id = 'flag-btn'
+  flagButton.innerText = '🚩'
+
+  gameDiv.append(flagButton)
+
+  flagButton.addEventListener('click', ()=>{
+    if(!flagEnabled){
+      flagButton.classList.add('flag-btn-active')
+      flagEnabled = true;
+    }else{
+      flagButton.classList.remove('flag-btn-active')
+      flagEnabled = false;
+    }
+    
+  })
+}
+
+function cellClicked() {
+  let cell = this;
+
+  if (flagEnabled){
+    if(!cell.innerText){
+      cell.innerText = '🚩'
     }
   }
 }
@@ -23,4 +56,8 @@ export const startgame = () =>{
   document.getElementById('start-game-btn').remove()
 
   createBoard();
+  createFlagButton();
 }
+
+
+
