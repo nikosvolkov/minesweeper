d56ec2a096eafb06a35219ddf7aecdb6641b617f
Volkov Nikita <crystalnik123@gmail.com>
2024-04-16 13:26:05 +0300

    Working on settings

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,7 +1,12 @@
-export const settingsHandler = () => {
 
+if (window.location.href == 'index.html'){
+  const homeButton = document.getElementById('home-button')
+  homeButton.addEventListener('click', () =>{
+    console.log('home');
+    window.location.href = 'index.html'
+  })
 }
 
 export const clickSettingsButton = () =>{
-  
+  window.location.href = 'settings.html'
 }
