ae8488fddebd13432e7a76f460026a26ee0c8499
Volkov Nikita <crystalnik123@gmail.com>
2024-04-18 00:11:20 +0300

    added input range, limits, mines amout related to board size

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,19 +1,94 @@
-const homeButton = document.getElementById('home-button')
+let currentWidthValue = null;
+let currentHeightValue = null;
+
+createInputPlaceholders();
+
+const homeButton = document.getElementById('home-button');
 homeButton.addEventListener('click', () => {
-  window.location.href = 'index.html'
-})
+  window.location.href = 'index.html';
+});
 
 const buttonActions = {
-  plus(input){input.value++},
-  minus(input){input.value--}
-}
+  plus(input, sizeName) {
+    let currentValue = parseInt(input.value);
+    const maxValue = parseInt(input.getAttribute('max'));
 
-const buttonCountainer = document.querySelector('.change-board-size')
-buttonCountainer.addEventListener('click', (event) =>{
-  const buttonElement = event.target.closest('button')
+    if (currentValue < maxValue) {
+      input.value++;
+      currentValue = parseInt(input.value);
+    }
+
+    if (sizeName == 'width') {
+      currentWidthValue = currentValue;
+    } else if (sizeName == 'height') {
+      currentHeightValue = currentValue;
+    } else return;
+  },
+  minus(input, sizeName) {
+    let currentValue = parseInt(input.value);
+    const minValue = parseInt(input.getAttribute('min'));
+
+    if (currentValue > minValue) {
+      input.value--;
+      currentValue = parseInt(input.value);
+    }
+
+    if (sizeName == 'width') {
+      currentWidthValue = currentValue;
+    } else if (sizeName == 'height') {
+      currentHeightValue = currentValue;
+    } else return;
+  },
+};
+
+const buttonCountainer = document.querySelector('.change-board-size');
+buttonCountainer.addEventListener('click', (event) => {
+
+  const buttonElement = event.target.closest('button');
   if (buttonElement == null) return;
   const operation = buttonElement.dataset.operation;
-  const relatedInput = event.target.parentNode.querySelector('input')
-  buttonActions[operation](relatedInput)
-})
+  const relatedInput = event.target.parentNode.querySelector('input');
+  const minValue = parseInt(relatedInput.getAttribute('min'));
+  const sizeName = relatedInput.parentNode.previousElementSibling.id;
+  const isInputNaN = isNaN(parseInt(relatedInput.value));
+  if (sizeName == 'width' && isInputNaN) {
+    currentWidthValue = minValue;
+  } else if (sizeName == 'height' && isInputNaN) {
+    currentHeightValue = minValue;
+  }
+  if (isInputNaN) {
+    relatedInput.value = minValue;
+    setMinesRangeValue();
+    return;
+  }
 
+  buttonActions[operation](relatedInput, sizeName);
+  setMinesRangeValue();
+});
+
+
+function setMinesRangeValue() {
+  if (currentHeightValue == null || currentWidthValue == null) return;
+  const minesInput = document.getElementById('mines-input');
+  const minMines = 1;
+  const maxMines = Math.round(
+    currentHeightValue * currentWidthValue -
+    (currentHeightValue * currentWidthValue * 40) / 100
+  );
+
+  minesInput.setAttribute('min', minMines);
+  minesInput.setAttribute('max', maxMines);
+  createInputPlaceholders();
+  console.log('Max Mines: ' + maxMines);
+}
+
+
+function createInputPlaceholders() {
+  const pageInputs = document.querySelectorAll('input');
+  pageInputs.forEach((input) => {
+    if (!input.hasAttribute('min')) return;
+    const minValue = input.getAttribute('min');
+    const maxValue = input.getAttribute('max');
+    input.setAttribute('placeholder', `${minValue}-${maxValue}`);
+  });
+}
