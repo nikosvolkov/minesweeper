b308130f0699d8fce4c48b0ece07c0d1f1908589
Volkov Nikita <crystalnik123@gmail.com>
2024-04-30 01:35:36 +0300

    added settings.html to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
-export default{
-  base: '/minesweeper/'
-}
+import { defineConfig } from "vite"
+import { resolve } from "path";
+export default defineConfig({
+  base: '/minesweeper/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        settings: resolve(__dirname, "settings.html"),
+      },
+    },
+  },
+});
