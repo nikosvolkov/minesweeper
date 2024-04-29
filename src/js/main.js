d2d885627228480c0b25745b18649bdc256336ab
Volkov Nikita <crystalnik123@gmail.com>
2024-04-29 23:45:07 +0300

    kek

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,5 @@ startGameButton.addEventListener('click', startgame);
 
 const settingsButton = document.getElementById('settings-btn');
 settingsButton.addEventListener('click', () => {
-  window.location.href = 'settings.html';
+  window.location.href = './settings.html';
 });
